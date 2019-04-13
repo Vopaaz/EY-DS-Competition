82175e58d9b1bb30dc7f3714229292c8bf7d2563
Vopaaz <liyifan945@163.com>
2019-04-13 21:44:49 +0800

    Finish: applicable fill_path algorithm

--- Solution/fill_path.py
+++ Solution/fill_path.py
@@ -1,77 +1,39 @@
 import pandas as pd
-import util
-
-rDF = util.Raw_DF_Reader()
-mainDF = rDF.test
-hashSeries = mainDF['hash'].drop_duplicates()
-hashSeries.index = list(range(0,len(hashSeries)))
-
-'''
-df = mainDF[mainDF['hash'] == '00032f51796fd5437b238e3a9823d13d_31']
-
-for j in range(0,100):
-    if(df.loc[j,'time_exit']!= df.loc[j+1,'time_entry']):
-
-        insertRow = pd.DataFrame([[df.loc[j,'hash'],df.loc[j,'trajectory_id'],df.loc[j,'time_exit'],df.loc[j+1,'time_entry'],
-                                   'nan','nan','nan',df.loc[j,'x_exit'],df.loc[j,'y_exit'],df.loc[j+1,'x_entry'],df.loc[j+1,'y_entry']]],
-                                 columns=['hash', 'trajectory_id', 'time_entry', 'time_exit', 'vmax', 'vmin',
-                                          'vmean', 'x_entry', 'y_entry', 'x_exit', 'y_exit'])
-
-        #print(insertRow)
-        above = df.loc[:j]
-        below = df.loc[j+1:]
-        df = above.append(insertRow,ignore_index = True).append(below,ignore_index = True)
-    if(j >= len(df)-2):
-        break
+from util import Raw_DF_Reader
+from sklearn.base import TransformerMixin, BaseEstimator
+import numpy as np
 
 
-df.to_csv('temp.csv')
-'''
-'''
-df = mainDF[mainDF['hash'] == '000479418b5561ab694a2870cc04fd43_25']
-df.index = range(0,len(df))
-for j in range(0, 100):
-    if (j < len(df) - 1):
-        if (df.loc[j, 'time_exit'] != df.loc[j + 1, 'time_entry']):
-            insertRow = pd.DataFrame(
-                [[df.loc[j, 'hash'], df.loc[j, 'trajectory_id'], df.loc[j, 'time_exit'], df.loc[j + 1, 'time_entry'],
-                  'nan', 'nan', 'nan', df.loc[j, 'x_exit'], df.loc[j, 'y_exit'], df.loc[j + 1, 'x_entry'],
-                  df.loc[j + 1, 'y_entry']]],
-                columns=['hash', 'trajectory_id', 'time_entry', 'time_exit', 'vmax', 'vmin',
-                         'vmean', 'x_entry', 'y_entry', 'x_exit', 'y_exit'])
+class FillPathTransformer(TransformerMixin, BaseEstimator):
+    def fit(self, X):
+        return self
 
-            above = df.loc[:j]
-            below = df.loc[j + 1:]
-            df = above.append(insertRow, ignore_index=True).append(below, ignore_index=True)
-    else:
-        break
+    def transform(self, X):
+        return pd.concat([self.__connect_one_device(i) for i in X.groupby("hash")], axis=0).reset_index(drop=True)
 
-df.to_csv('temp.csv')
-'''
+    def __connect_one_device(self, group):
+        hash_, df = group[0], group[1]
 
-fullDF = pd.DataFrame([], columns=['hash', 'trajectory_id', 'time_entry', 'time_exit', 'vmax', 'vmin',
-                             'vmean', 'x_entry', 'y_entry', 'x_exit', 'y_exit'])
-#grouped = mainDF.groupby(mainDF['hash'])
-for i in mainDF['hash']:
-    df = mainDF[mainDF['hash'] == i]
-    df.index = range(0,len(df))
-    for j in range(0, 100):
-        if(j < len(df)-1):
-            if (df.loc[j, 'time_exit'] != df.loc[j + 1, 'time_entry']):
-                insertRow = pd.DataFrame(
-                    [[df.loc[j, 'hash'], df.loc[j, 'trajectory_id'], df.loc[j, 'time_exit'], df.loc[j + 1, 'time_entry'],
-                      'nan', 'nan', 'nan', df.loc[j, 'x_exit'], df.loc[j, 'y_exit'], df.loc[j + 1, 'x_entry'],
-                      df.loc[j + 1, 'y_entry']]],
-                    columns=['hash', 'trajectory_id', 'time_entry', 'time_exit', 'vmax', 'vmin',
-                             'vmean', 'x_entry', 'y_entry', 'x_exit', 'y_exit'])
+        def rename_traj_id(x):
+            return x + ".5"
 
-                above = df.loc[:j]
-                below = df.loc[j + 1:]
-                df = above.append(insertRow, ignore_index=True).append(below, ignore_index=True)
-        else:
-            break
-    #print(df)
-    fullDF = fullDF.append(df, ignore_index=True)
-    #print(fullDF)
+        new_df = pd.DataFrame([[
+            hash_,
+            rename_traj_id(df.iloc[i, 1]),
+            df.iloc[i, 3],
+            df.iloc[i+1, 2],
+            np.nan,
+            np.nan,
+            np.nan,
+            df.iloc[i, 9],
+            df.iloc[i, 10],
+            df.iloc[i+1, 7],
+            df.iloc[i+1, 8]
+        ] for i in range(df.shape[0] - 1)], columns=df.columns)
 
-fullDF.to_csv('fullDF.csv')
+        return pd.concat([df,new_df], axis=0).sort_values(by="time_entry", ascending=True)
+
+
+if __name__ == "__main__":
+    df = Raw_DF_Reader().test
+    print(FillPathTransformer().fit_transform(df))
