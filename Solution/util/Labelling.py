514ac389f6483b7162565352b2da28c4b49ec31e
Vopaaz <liyifan945@163.com>
2019-04-17 00:12:15 +0800

    HotFix: Wrong Labelling

--- Solution/util/Labelling.py
+++ Solution/util/Labelling.py
@@ -5,10 +5,19 @@ from util.utilFunc import isin_center
 
 
 class Labeller(TransformerMixin, BaseEstimator):
+    '''
+        Label the train set.
+    '''
+
     def fit(self, X):
         return self
 
     def transform(self, X):
+        '''
+            Parameters: a DataFrame containing column "hash", "x_exit", "y_exit".
+
+            Returns: a DataFrame of "hash" numbers of rows, one column "target".
+        '''
         res = pd.DataFrame(X.groupby("hash").apply(
             lambda series: isin_center(
                 series.x_entry.iloc[-1], series.y_entry.iloc[-1]),
