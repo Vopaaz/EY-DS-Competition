90583f0e27ef4e13c5d9b324b4a50ff36467ebfd
Vopaaz <liyifan945@163.com>
2019-04-16 13:53:17 +0800

    Adjust Project Structure

--- Solution/util/NaiveFeature.py
+++ Solution/util/NaiveFeature.py
@@ -4,8 +4,7 @@ import numpy as np
 import pandas as pd
 from sklearn.base import BaseEstimator, TransformerMixin
 
-from util import Raw_DF_Reader, distance_to_border, distance_between, time_delta
-from PathFilling import FillPathTransformer
+from util.utilFunc import Raw_DF_Reader, distance_to_border, distance_between, time_delta
 
 
 class DistanceInfoExtractor(TransformerMixin, BaseEstimator):
