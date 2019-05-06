ef54e4104e32324c0337cf237973fc1eb0283321
Vopaaz <liyifan945@163.com>
2019-05-06 15:38:23 +0800

    Update Leinao Adjustment

--- Useless/leinao_io_dir_gist.py
+++ Useless/leinao_io_dir_gist.py
@@ -1,4 +1,4 @@
-# Solution\DFPreparation.py, line 108-115
+# Solution\Machine\DFPreparation.py, line 108-115
 def __get_filepath(self):
     '''
         Map the parameters of the provider to an identifiable filepath.
@@ -8,7 +8,7 @@ def __get_filepath(self):
         ("-pathfilled" if self.path_filled else "") + ".csv"
     return os.path.join(dir_, fname)
 
-# Solution\util\utilFunc.py, line 26-41
+# Solution\util\BaseUtil.py, line 26-41
 def __get_raw_test(self):
     r'''
         Read the raw test data table.
@@ -25,3 +25,23 @@ def __get_raw_train(self):
     '''
     with open(r"/input_dir/datasets/eyds/data_train.csv", "r", encoding="utf-8") as f:
         self.train = pd.read_csv(f, index_col=0)
+
+# Solution\util\Submition.py, line 39-56
+def save(self, memo=""):
+    '''
+        Save the result DataFrame to csv file.
+        The target diretory is "Result". The file will be named by monthday-hour-minute-second.
+
+        Parameters:
+            - memo: A string that describes this result DataFrame, it will be written in the memo.txt under the Result dir.
+    '''
+    filename = datetime.datetime.now().strftime(r"%m%d-%H-%M-%S") + ".csv"
+    filepath = os.path.join("output_dir", filename)
+    self.result.to_csv(filepath, encoding="utf-8",
+                        index=False, line_terminator="\n")
+
+    with open(os.path.join("output_dir", "memo.txt"), "a+", encoding="utf-8") as f:
+        f.write(filename)
+        f.write("\t")
+        f.write(str(memo))
+        f.write("\n")
